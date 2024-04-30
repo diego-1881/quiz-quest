05d5a1e0aef82f31956d4fa74359a8d0eed1905e
diego1881 <hola@diegoesteban.com>
2024-04-30 10:36:42 -0500

    move components to separate folder and add questions component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
-import Loader from "./Loader";
+import Loader from "../Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
@@ -10,6 +10,7 @@ import Question from "./Question";
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -36,7 +37,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
@@ -55,7 +56,7 @@ export default function App() {
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
         {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
