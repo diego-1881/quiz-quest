01358523c2244824f2f982135803d34dffcce469
diego1881 <hola@diegoesteban.com>
2024-05-08 07:52:01 -0500

    add favicon and update styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="logo512.png" alt="React logo" />
       <h1>The Quiz Quest</h1>
+      <span className="symbol">🎯</span>
     </header>
   );
 }
